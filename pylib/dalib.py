b57b592270ef51f73d1a9cd2f2ba3d161b435bc5
Gibies George <gibies.george@gov.in>
2026-01-15 20:50:09 +0530

    Updated 'aiesda' for ''

---
+++ pylib/dalib.py
@@ -0,0 +1,27 @@
+import numpy
+
+class JEDI_Interface:
+    """Interface class to be used by the JEDI-OOPS wrapper."""
+    
+    def __init__(self, config):
+        self.config = config
+        self.geometry = self._setup_geometry()
+
+    def _setup_geometry(self):
+        # Define grid based on NCMRWF standards (e.g., IMDAA 12km)
+        pass
+
+    def apply_observation_operator(self, state):
+        """Equivalent to H(x) in JEDI."""
+        # Call your AI model from ailib here
+        # return simulated_observations
+        pass
+
+    def compute_cost_gradient(self, state, observations):
+        """Calculates grad(J) for the JEDI minimizer."""
+        # Logic for dJ/dx
+        pass
+
+def jedi_bind_state(state_data):
+    """Utility to convert AI tensors to JEDI-compatible State objects."""
+    return {"data": state_data, "metadata": "NCMRWF-AIESDA-v1"}
