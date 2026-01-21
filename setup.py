f286cb5d4d318c2349107d1106e716076aea37af
Gibies George <gibies.george@gov.in>
2026-01-21 17:58:39 +0530

    Updated 'aiesda' for ''

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ setup(
     description="Artificial Intelligence based Earth System Data Assimilation",
     author="gibies",
     # We define the sub-packages within the aiesda namespace
-    packages=["aiesda", "aiesda.dic", "aiesda.scripts"],
+    packages=["aiesda.pylib", "aiesda.pydic", "aiesda.scripts", "aiesda.jobs"],
     # We map those namespaces to your physical folder names
     package_dir={
-        "aiesda": "pylib",
-        "aiesda.dic": "pydic",
+        "aiesda.pylib": "pylib",
+        "aiesda.pydic": "pydic",
         "aiesda.scripts": "scripts"
+        "aiesda.jobs": "jobs"
+        "aiesda.nml": "nml"
+        "aiesda.yaml": "yaml"
     },
     # This handles the non-python files (yml, nml) if needed later
     include_package_data=True,
