e96fa82816e12098958e0635ddb43ce75d2fa870
Gibies George <gibiesgeorge@gmail.com>
2026-01-23 05:37:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,16 +173,12 @@ RUN if ! command -v python3 >/dev/null 2>&1; then \
 RUN apt-get update && apt-get install -y python3-pip libeccodes-dev && \
     rm -rf /var/lib/apt/lists/*
 
-# 2. DYNAMIC PATH SETUP (Fixes the Python 3.10 vs 3.12 issue)
-# This finds the correct site-packages folder regardless of Python version
-RUN P_VER=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')") && \
-    echo "export PYTHONPATH=/usr/local/lib/python\${P_VER}/dist-packages:/usr/local/lib:/home/aiesda/lib/aiesda/pylib:/home/aiesda/lib/aiesda/pydic:\${PYTHONPATH}" >> /etc/bash.bashrc
-
-# 3. Set environment for JEDI and AIESDA
-# We include both the site-packages for UFO and the local AIESDA paths
-ENV PYTHONPATH="/usr/local/lib/python3.12/dist-packages:/usr/local/lib:/home/aiesda/lib/aiesda/pylib:/home/aiesda/lib/aiesda/pydic:\${PYTHONPATH}"
-ENV LD_LIBRARY_PATH="/usr/local/lib:\${LD_LIBRARY_PATH}"
-ENV PATH="/usr/bin:/usr/local/bin:\${PATH}"
+# 2. SMART PATH SETUP
+# We add both the system site-packages AND the AIESDA folders to the path.
+# We use a wildcard (*) for the python version to make it version-agnostic.
+ENV PYTHONPATH="/usr/local/lib/python3.*/dist-packages:/usr/local/lib:/home/aiesda/lib/aiesda/pylib:/home/aiesda/lib/aiesda/pydic"
+ENV LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
+ENV PATH="/usr/bin:/usr/local/bin:${PATH}"
 
 WORKDIR /home/aiesda
 COPY requirement.txt .
