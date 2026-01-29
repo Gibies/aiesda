e04dc2282d74447c57b98b6386a16bf9c80b25ef
Gibies George <gibiesgeorge@gmail.com>
2026-01-29 10:15:19 +0530

    Update install.sh

--- jobs/install.sh
+++ jobs/install.sh
@@ -3,13 +3,45 @@
 # AIESDA Unified Installer (WSL/Laptop & HPC)
 # ==============================================================================
 # install.sh
+
+SELF=$(realpath ${0})
+HOST=$(hostname)
+export JOBSDIR=${SELF%/*}
+export PKG_ROOT=${SELF%/jobs/*}
+export PKG_NAME=${PKG_ROOT##*/}
+
+###########################################################################################
+helpdesk()
+{
+echo -e "Usage: \n $0"
+                        echo "options:"
+			echo "-h	--help		Help"
+			echo "-s	--site		site information for coustum settings"
+			echo "-p	--pkg		Package Name"
+                        exit 0
+}
+###########################################################################################
+options()
+{
+while test $# -gt 0; do
+     case "$1" in
+            -h|--help) 	helpdesk;;
+		    -s|--site)	shift; SITE_NAME=$1; shift;;
+		    -p|--pkg)	shift; PKG_NAME=$1; shift;;
+		    *)		shift;;
+	esac
+done
+}
+###########################################################################################
+options $(echo $@  | tr "=" " ")
+###########################################################################################
 ###########################################################
 # --- 1. Configuration ---
 ###########################################################
 
-PROJECT_NAME="aiesda"
+PROJECT_NAME=${PKG_NAME:-"aiesda"}
 # Capture Site Argument (Default to 'docker')
-SITE_NAME=${1:-"docker"}
+SITE_NAME=${SITE_NAME:-"docker"}
 
 # Discover the Repo Root relative to this script's location
 # This allows you to run 'bash jobs/install.sh' from anywhere
